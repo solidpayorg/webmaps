9e1856d620d8ca9102d0e14a52701cc2de428c9f
Melvin Carvalho <melvincarvalho@gmail.com>
2019-10-08 16:23:50 +0200

    make image map clickable

--- bin/processhtml.sh
+++ bin/processhtml.sh
@@ -10,4 +10,26 @@ do
 
     mv /tmp/$$ "$FILE"
 
+    IDS=$(grep 'href=..FMID' "$FILE" | sed 's/.*"#\(FMID[^"]*\)".*/\1/')
+
+    for ID in $(grep 'href=..FMID' "$FILE" | sed 's/.*"#\(FMID[^"]*\)".*/\1/')
+    do
+      #echo "$ID"
+      HREF=$(grep "\"$ID" "$FILE" | sed "s/.*\($ID\)\"..[^/]*href=.\([^\"]*\)\".*/\2/") 
+      2>&1 echo $HREF | grep nodecontent | grep border-width > /dev/null
+      #echo $?
+      if [[ $? -eq 1 ]]
+      then
+        echo $ID
+        echo $HREF
+        sed "s,#$ID,$HREF," "$FILE" > /tmp/$$
+        mv /tmp/$$ "$FILE"
+      fi
+      
+    done
+
 done
+
+# <a id="FMID_101804111FM" /><div class="nodecontent" style="color:#000000;font-size:83%;"><a href="https://angular.io/">
+
+# <area shape="rect" href="#FMID_101804111FM" alt="angular" title="angular" coords="547,136,622,154" />

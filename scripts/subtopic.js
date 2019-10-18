524a413b1c95101390a54143fa393ab77e569106
Melvin Carvalho <melvincarvalho@gmail.com>
2019-10-18 02:16:58 +0200

    toggle subtopic

---
+++ scripts/subtopic.js
@@ -0,0 +1,29 @@
+if (node.style && node.style.name && node.style.name === 'styles.subsubtopic') {
+  node.style.name = null
+} else {
+  node.style.name = 'styles.subsubtopic'
+}
+
+// styles.AutomaticLayout = Samodejna postavitev
+// styles.connection = Povezanost
+// styles.date = Datum
+// styles.definition = Definicija
+// styles.description = Opis
+// styles.floating_node = Plavajo\u010De vozli\u0161\u010De
+// styles.idea = Ideja
+// styles.important = Pomembno
+// styles.key = Klju\u010D
+// styles.list = Seznam
+// styles.needs_action = Potrebno je ukrepanje
+// styles.note = Opomba
+// styles.ok = V redu
+// styles.pending = V teku
+// styles.predefined = Vnaprej dolo\u010Deni slogi
+// styles.question = Vpra\u0161anje
+// styles.quotation = Citat
+// styles.root_node = Slogi
+// styles.subsubtopic = Pod - podtema
+// styles.subtopic = Podtema
+// styles.topic = Tema
+// styles.user - defined=Uporabni\u0161ko definirani slogi
+// styles.website = Spletna stran

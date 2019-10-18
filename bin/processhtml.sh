dd07480cd2583924cbde2be4feeac94274c75ade
Melvin Carvalho <melvincarvalho@gmail.com>
2019-10-18 17:22:23 +0200

    fix large hashtag images

--- bin/processhtml.sh
+++ bin/processhtml.sh
@@ -16,7 +16,7 @@ do
     fi
 
 
-    sed 's/\.mm"/.html"/g' "$FILE"  | sed 's/\.png" a/.svg" a/g' | sed 's/\ilink.svg/ilink.png/g' | sed 's/freeplane2html.xsl/processhtml.sh/' > /tmp/$$
+    sed 's/\.mm"/.html"/g' "$FILE"  | sed 's/\.png" a/.svg" a/g' | sed 's/ilink.svg/ilink.png/g' | sed 's/hashtag.svg/hashtag.png/g' | sed 's/freeplane2html.xsl/processhtml.sh/' > /tmp/$$
 
     mv /tmp/$$ "$FILE"
 

59d82b842f855b9f328c8ffeb570f1762e68d6e2
Melvin Carvalho <melvincarvalho@gmail.com>
2020-01-19 15:17:41 +0100

    add docs

---
+++ docs/mindmaps.mm
@@ -0,0 +1,127 @@
+<map version="freeplane 1.7.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="mindmaps" FOLDED="false" ID="ID_379023905" CREATED="1569782968766" MODIFIED="1579443365722" LINK="index.mm" STYLE="oval">
+<edge STYLE="bezier" COLOR="#7c007c" WIDTH="thin" DASH="SOLID"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="14" RULE="ON_BRANCH_CREATION"/>
+<font SIZE="18"/>
+<node TEXT="Software" POSITION="right" ID="ID_466531788" CREATED="1579442908856" MODIFIED="1579443029828">
+<edge COLOR="#007c7c"/>
+<font BOLD="true"/>
+<node TEXT="Mindmaps are created using the desktop app freeplane" ID="ID_1487889840" CREATED="1579442916695" MODIFIED="1579442938886"/>
+<node TEXT="Download it here" ID="ID_1358046872" CREATED="1579442955647" MODIFIED="1579442958218"/>
+<node ID="ID_1209244642" CREATED="1579442964557" MODIFIED="1579442964557" LINK="https://sourceforge.net/projects/freeplane/"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <a http-equiv="content-type" content="text/html; charset=utf-8" href="https://sourceforge.net/projects/freeplane/">https://sourceforge.net/projects/freeplane/</a> 
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+<node TEXT="Create" POSITION="right" ID="ID_1669457890" CREATED="1579442969564" MODIFIED="1579443029242">
+<edge COLOR="#7c7c00"/>
+<font BOLD="true"/>
+<node TEXT="Base mind maps are crated using the desktop sofware" ID="ID_1462942176" CREATED="1579442981688" MODIFIED="1579443002922"/>
+<node TEXT="Intuitive user interface gets you started quicky creating mind maps" ID="ID_1170903995" CREATED="1579443004604" MODIFIED="1579443019996"/>
+</node>
+<node TEXT="Publish" POSITION="right" ID="ID_1387369191" CREATED="1579443021324" MODIFIED="1579443028578">
+<edge COLOR="#ff0000"/>
+<font BOLD="true"/>
+<node TEXT="Mind maps are published using the patterns at webmaps.io" ID="ID_1977216947" CREATED="1579443032002" MODIFIED="1579443320693"/>
+<node ID="ID_963916441" CREATED="1579443051438" MODIFIED="1579443051438" LINK="https://webmaps.io/"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <a http-equiv="content-type" content="text/html; charset=utf-8" href="https://webmaps.io/">https://webmaps.io/</a> 
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="Can export to HTML" ID="ID_727557840" CREATED="1579443054728" MODIFIED="1579443059218"/>
+<node TEXT="Can then publish to a web server, e.g. using rsync" ID="ID_1758466931" CREATED="1579443060915" MODIFIED="1579443332331"/>
+</node>
+<node TEXT="Slides" POSITION="right" ID="ID_1592562658" CREATED="1579443098296" MODIFIED="1579443104964">
+<edge COLOR="#0000ff"/>
+<font BOLD="true"/>
+<node TEXT="It is possible to turn a mind map into slides" ID="ID_675897023" CREATED="1579443106941" MODIFIED="1579443114832"/>
+<node TEXT="mindslide" ID="ID_1580713250" CREATED="1579443131608" MODIFIED="1579443193365" LINK="http://berteh.github.io/mindslide/#/ID_1723255651"/>
+<node TEXT="Mindslide is used to create presentations" ID="ID_1135894441" CREATED="1579443194378" MODIFIED="1579443207518"/>
+</node>
+<node TEXT="Automate" POSITION="right" ID="ID_567201254" CREATED="1579443155420" MODIFIED="1579443159531">
+<edge COLOR="#00ff00"/>
+<font BOLD="true"/>
+<node TEXT="TODO" ID="ID_804280246" CREATED="1579443160801" MODIFIED="1579443175215"/>
+<node TEXT="Describe automation process for manual steps" ID="ID_629481915" CREATED="1579443176421" MODIFIED="1579443186253"/>
+</node>
+</node>
+</map>
